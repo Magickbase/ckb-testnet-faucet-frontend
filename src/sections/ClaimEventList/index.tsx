8b38b5f69bb44ba3e6fbf1a40978f3aa166894c9
IronLu233 <ironlu233.dev@outlook.com>
2023-06-09 15:51:20 +0800

    fix: layout on small screen

--- src/sections/ClaimEventList/index.tsx
+++ src/sections/ClaimEventList/index.tsx
@@ -8,27 +8,29 @@ export const ClaimEventList: FC = () => {
   const { data: claimEvents } = useClaimEvents(queryAddress);
 
   return (
-    <div className="w-full max-w-[880px]  box-content px-4">
-      <div className="flex justify-between w-full items-center py-8">
-        <h2 className=" text-3xl">Claims</h2>
-        <div className="relative">
-          <input
-            placeholder="Search Address"
-            className="border rounded border-gray-200 placeholder:text-gray-400 text-sm px-2 py-3 pr-8 h-10 w-[196px]"
-            value={queryAddress}
-            onChange={(e) => setQueryAddress(e.target.value)}
-          />
-          {!!queryAddress && (
-            <CloseIcon
-              className="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer"
-              onClick={() => setQueryAddress("")}
+    <div className="px-4 w-full flex justify-center">
+      <div className="w-full max-w-[880px]">
+        <div className="flex justify-between w-full items-center py-8">
+          <h2 className=" text-3xl">Claims</h2>
+          <div className="relative">
+            <input
+              placeholder="Search Address"
+              className="border rounded border-gray-200 placeholder:text-gray-400 text-sm px-2 py-3 pr-8 h-10 w-[196px]"
+              value={queryAddress}
+              onChange={(e) => setQueryAddress(e.target.value)}
             />
-          )}
+            {!!queryAddress && (
+              <CloseIcon
+                className="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer"
+                onClick={() => setQueryAddress("")}
+              />
+            )}
+          </div>
         </div>
+        {claimEvents.map((event) => (
+          <ClaimEventItem key={event.id} event={event} />
+        ))}
       </div>
-      {claimEvents.map((event) => (
-        <ClaimEventItem key={event.id} event={event} />
-      ))}
     </div>
   );
 };
